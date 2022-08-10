70a5dc9ad9a3093ed7b43135d91eb3cd9ba5510d
alemartinn <ale_xeg147@hotmail.com>
2022-08-10 18:25:25 -0300

    mejoras de card

--- js/productos.js
+++ js/productos.js
@@ -1,33 +1,62 @@
 
+// const printCardsCarrito = (productos, container) => {
+
+//     const ultimasUnidades = "<span class='text-dark fw-bold' >Ultimas Unidades!</span>"
+
+//     productos.forEach(producto => {
+//         let card = document.createElement('div')
+//         card.className = 'card shadow'
+//         card.style.width = '18rem'
+//         card.style.minWidth = '300px'
+//         card.innerHTML = `
+//         <img src="${producto.imagen}" style="height: 50%; border-bottom: 1px solid rgba(0,0,0,0.08);" class="card-img-top" alt="producto">
+//         <div class="card-body" style="height: 50%;">
+//             <h5 class="card-title">${producto.nombre}</h5>
+//         </div>
+//         <ul class="list-group list-group-flush">
+//             <li class="list-group-item">Precio: $${producto.precio}</li>
+//             <li class="list-group-item d-flex justify-content-between">Stock: ${producto.stock} ${producto.stock < 3 ? ultimasUnidades : ""}</li>
+//         </ul>
+//         <div class="card-body d-flex justify-content-around">
+//             <div class="d-flex justify-content-evenly align-items-center">
+//                 <button onClick=disminuirCantidad("${producto._id}") class="boton botonRestar" > - </button>
+//                 <span id=${producto._id} class="text-center" style="width: 30px;"> 0 </span>
+//                 <button onClick=aumentarCantidad("${producto._id}","${producto.stock}")> + </button>
+//             </div>
+//             <a href="./carrito.html" class="btn btn-outline-dark card-link">Comprar</a>
+//         </div>
+//         `
+//         container.appendChild(card)
+//     })
+// }
+
+const aumentarCantidad = (idProducto, stockTotal) => {
+    let valorElegidoText = document.getElementById(idProducto);
+    let valorElegido = parseInt(valorElegidoText.textContent);
+    if (valorElegido < stockTotal){
+        valorElegido++;
+    }
+    valorElegidoText.textContent = valorElegido;
+}
+
+const disminuirCantidad = (idProducto) => {
+    let valorElegidoText = document.getElementById(idProducto);
+    let valorElegido = parseInt(valorElegidoText.textContent);
+    if (valorElegido > 0){
+        valorElegido--;
+    }
+    valorElegidoText.textContent = valorElegido;
+}
+
 const getFilteredProducts = (datosProductos, tipoProducto) => {
     return datosProductos.filter(producto => producto.tipo == tipoProducto)
 }
-
-/* <div class="card" style="width: 18rem;">
-    <img src="..." class="card-img-top" alt="producto">
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    </div>
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item">An item</li>
-        <li class="list-group-item">A second item</li>
-    </ul>
-    <div class="card-body">
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
-    </div>
-</div> */
 
 const printCards = (productos, container) => {
 
     const ultimasUnidades = "<span class='text-dark fw-bold' >Ultimas Unidades!</span>"
 
     productos.forEach(producto => {
-
-        if (parseInt(producto.stock) < 3){
-            
-        }
         let card = document.createElement('div')
         card.className = 'card shadow'
         card.style.width = '18rem'
@@ -42,14 +71,10 @@ const printCards = (productos, container) => {
             <li class="list-group-item d-flex justify-content-between">Stock: ${producto.stock} ${producto.stock < 3 ? ultimasUnidades : ""}</li>
         </ul>
         <div class="card-body d-flex justify-content-around">
-            <div class="d-flex justify-content-evenly align-items-center">
-                <button onClick=disminuirCantidad()>-</button>
-                <span class="text-center" style="width: 30px;"> 1 </span>
-                <button onClick=aumentarCantidad()>+</button>
-            </div>
-            <a href="#" class="btn btn-outline-dark card-link">Comprar</a>
+            <a href="./carrito.html" class="btn btn-outline-dark card-link">Agregar a carrito</a>
+            <a href="./carrito.html" class="btn btn-outline-dark card-link">Comprar</a>
         </div>
-        `    
+        `
         container.appendChild(card)
     })
 }
