a548f214db9420312d27310554abf7d7c7e4cf71
alemartinn <ale_xeg147@hotmail.com>
2022-08-16 21:52:58 -0300

    mejoras de formulario

--- js/contact.js
+++ js/contact.js
@@ -1,22 +1,23 @@
 
 const mostrarAvisoFormulario = (e) => {
-    e.preventDefault()
-    let contenedorAlerta = document.getElementById('contenedorAlerta')
+    e.preventDefault();
+
+    let contenedorAlerta = document.getElementById('contenedorAlerta');
     contenedorAlerta.innerHTML=`
     <div class="alert alert-success m-2" style="min-height: 7vh;" role="alert" id="miAlertaContact">
         <b>¡Su mensaje ha sido enviado correctamente, gracias por contactarnos!</b>
     </div>
-    `
+    `;
     setTimeout(() => {
     contenedorAlerta.innerHTML=`
     <div class="alert alert-success m-2" style="min-height: 7vh; display: none;" role="alert" id="miAlertaContact">
         Su mensaje ha sido enviado correctamente, gracias por contactarnos!
     </div>
-    `
-    },3000)
+    `;
+    },3000);
     
-    document.getElementById("myForm").reset()
+    document.getElementById("myForm").reset();
 }
 
 let botonEnviarForm = document.getElementById('botonAlerta');
-botonEnviarForm.addEventListener('click', (e) => mostrarAvisoFormulario(e))
+botonEnviarForm.addEventListener('click', (e) => mostrarAvisoFormulario(e));
